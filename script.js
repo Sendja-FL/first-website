b9dc80f2cc9c035eccee559b2b38643639c9a004
Bekiboo <julien.connault@gmail.com>
2024-08-08 10:14:03 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const tabs = document.querySelectorAll(".tabs__tab")
+
+const tabsSmartBar = document.querySelector(".tabs__smart-bar")
+
+tabsSmartBar.style.width = `${tabs[0].scrollWidth}px`
+
+tabs.forEach((tab) => tab.addEventListener("click", handelTabSwitch))
+
+function handelTabSwitch(e) {
+  console.log(e)
+  tabsSmartBar.style.width = `${e.target.scrollWidth}px`
+  tabsSmartBar.style.transform = `translateX(${e.target.offsetLeft}px)`
+}
